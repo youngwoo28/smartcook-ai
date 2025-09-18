365e71bbaba46269341d0a062c048852942eeace
ghddusghk46 <127215169+ghddusghk46@users.noreply.github.com>
2025-09-18 14:05:12 +0900

    0918

--- smartcook_backend/smartcook_backend/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import logout
-from django.shortcuts import redirect
-
-def logout_view(request):
-    if request.method == "POST":
-        logout(request)
-        return redirect("mainpage")  # 로그아웃 후 메인페이지 이동
-    return redirect("mainpage")  # 혹시 GET으로 접근해도 그냥 메인페이지로
