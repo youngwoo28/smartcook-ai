a1df176dca73562c7b4c59ae987dc5f53c9f6a83
ghddusghk46 <127215169+ghddusghk46@users.noreply.github.com>
2025-09-18 20:05:55 +0900

    0918

--- smartcook_backend/core/urls.py
+++ smartcook_backend/core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("signup/", signup, name="signup"),
     path("login/", views.login_view, name="login"),
     path("", views.main_page, name="mainpage"),
-    path("menu2/", views.menu2_view, name="menu2"),   # 마이페이지 (POST 저장 지원)
+    
     path("logout/", LogoutView.as_view(), name="logout"),
     path("upload/", views.upload_page, name="upload"),
     path("mypage/", views.mypage_view, name="mypage"),  # 혹시 단순 랜더링용 별도 필요할 경우
